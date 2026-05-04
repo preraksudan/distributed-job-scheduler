4cf96b15d143c6db10a42d7f6f2dbab6d7144dcd
preraksudan <preraksudan2@gmail.com>
2026-05-04 15:04:48 +0530

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "API running",
+		})
+	})
+
+	router.Run(":8080")
+}
